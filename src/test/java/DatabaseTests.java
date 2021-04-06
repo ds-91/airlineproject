5bbcf83697fbe4ca7f60cc95432aa065c8619409
quaide <tranterq@gmail.com>
2021-04-06 01:07:13 -0400

    Added Database Stub and test to Database test class

--- src/test/java/DatabaseTests.java
+++ src/test/java/DatabaseTests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,22 @@ public class DatabaseTests {
     Assertions.assertDoesNotThrow(() -> {
       dbm = new DatabaseManager();
     });
+  }
+
+  @Test
+  public void databaseStubTest()
+  {
+    byte [] userimage = new byte[]{};
+    Customer customer = new Customer("01", "first_name",
+            "last_name", "gender", "address",
+            "dob", "contact", userimage, "nic", "passport");
+    StubDatabase stubDatabase = new StubDatabase();
+    stubDatabase.insertCustomer(customer);
+
+
+    Assert.assertEquals("John", stubDatabase.getFirstName());
+    Assert.assertEquals("Smith", stubDatabase.getLastName());
+    Assert.assertEquals("Male", stubDatabase.getGender());
+    return;
   }
 }
--- src/test/java/StubDatabase.java
+++ src/test/java/StubDatabase.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class StubDatabase {
+public class StubDatabase {
+    String firstName;
+    String lastName;
+    String gender;
+
+    public void insertCustomer(Customer customer) {
+        this.firstName = customer.getFirstname();
+        this.lastName = customer.getLastname();
+        this.gender = customer.getGender();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
