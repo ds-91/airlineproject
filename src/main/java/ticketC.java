b868076fd6b781c95536041a13623c31091d3dfe
adhorne <34281624+adhorne@users.noreply.github.com>
2021-04-17 12:28:51 -0400

    Ticket Class and DAO

---
+++ src/main/java/ticketC.java
@@ -0,0 +1,77 @@
+public class ticketC {
+
+    private String id;
+    private String flightid;
+    private String custid;
+    private String classes;
+    private String price;
+    private String seats;
+    private String date;
+
+    ticketC (String id, String flightid, String custid, String classes, String price, String seats, String date ) {
+        this.id = id;
+        this.flightid = flightid;
+        this.custid = custid;
+        this.classes = classes;
+        this.price = price;
+        this.seats = seats;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFlightid() {
+        return flightid;
+    }
+
+    public void setFlightid(String flightid) {
+        this.flightid = flightid;
+    }
+
+    public String getCustid() {
+        return custid;
+    }
+
+    public void setCustid(String custid) {
+        this.custid = custid;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
--- src/main/java/ticketDAO.java
+++ src/main/java/ticketDAO.java
@@ -12,22 +12,34 @@ public class ticketDAO {
     }
 
     public boolean ticketC (ticketC ticket) {
-        if (ticket == null || ticket.checkForNullOrEmpty()){
+        if (ticket == null){
+            return false;
+        }
+
+        if (ticket.getId() == null || ticket.getId().isEmpty() ||
+                ticket.getId() == null || ticket.getId().isEmpty() ||
+                ticket.getFlightid() == null || ticket.getFlightid().isEmpty() ||
+                ticket.getCustid() == null || ticket.getCustid().isEmpty() ||
+                ticket.getClasses() == null || ticket.getClasses().isEmpty() ||
+                ticket.getPrice() == null || ticket.getPrice().isEmpty() ||
+                ticket.getSeats() == null || ticket.getSeats().isEmpty() ||
+                ticket.getDate() == null || ticket.getDate().isEmpty()) {
+
             return false;
         }
 
         Connection con = this.databaseManager.getDatabaseConnection();
         int success = 0;
         try {
-            PreparedStatement pst = con.prepareStatement("INSERT INTO" + "ticketC(id,flightid, custid, classes, " +
+            PreparedStatement pst = con.prepareStatement("INSERT INTO" + "ticket(id,flightid, custid, classes, " +
                     "price, seats, date) VALUES (?,?,?,?,?,?,?)");
-            pst.setString(1, ticketC.id());
-            pst.setString(2, ticketC.flightid());
-            pst.setString(3, ticketC.custid());
-            pst.setString(4, ticketC.classes());
-            pst.setString(5, ticketC.price());
-            pst.setString(6, ticketC.seats());
-            pst.setString(7, ticketC.date());
+            pst.setString(1, ticket.getId());
+            pst.setString(2, ticket.getFlightid());
+            pst.setString(3, ticket.getCustid());
+            pst.setString(4, ticket.getClasses());
+            pst.setString(5, ticket.getPrice());
+            pst.setString(6, ticket.getSeats());
+            pst.setString(7, ticket.getDate());
 
             success = pst.executeUpdate();
 
